b4d40a6aed56b01e5d4ab8af52fb97f13c788eea
edng0827 <66429126+edng0827@users.noreply.github.com>
2020-06-04 11:37:59 -0400

    Add files via upload

---
+++ Advent of Coding/src/main/java/Day1.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day1 {
+    public static String INPUT = "src/main/files/day1";
+    private static String NORTH = "NORTH";
+    private static String SOUTH = "SOUTH";
+    private static String EAST = "EAST";
+    private static String WEST = "WEST";
+
+    public static void main(String[] args){
+        
+    }
+}
